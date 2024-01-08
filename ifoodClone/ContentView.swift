b921cdfa17b1c89e3e997d675cd077f5eb75b147
LucasGatica <lucas.gatica79@gmail.com>
2024-01-08 18:28:43 -0300

    Initial Commit

---
+++ ifoodClone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ifoodClone
+//
+//  Created by Lucas Gaticão  on 08/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ifoodClone/ifoodCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ifoodCloneApp.swift
+//  ifoodClone
+//
+//  Created by Lucas Gaticão  on 08/01/24.
+//
+
+import SwiftUI
+
+@main
+struct ifoodCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
